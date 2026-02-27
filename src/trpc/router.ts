3e23da157d8e3f471685096cf44a85a1a3befd8f
TerrifiedBug <35064668+TerrifiedBug@users.noreply.github.com>
2026-02-27 18:41:44 +0000

    chore: wire fleet router into app router

--- src/trpc/router.ts
+++ src/trpc/router.ts
@@ -1,10 +1,12 @@
 import { router } from "./init";
 import { teamRouter } from "@/server/routers/team";
 import { environmentRouter } from "@/server/routers/environment";
+import { fleetRouter } from "@/server/routers/fleet";
 
 export const appRouter = router({
   team: teamRouter,
   environment: environmentRouter,
+  fleet: fleetRouter,
 });
 
 export type AppRouter = typeof appRouter;
