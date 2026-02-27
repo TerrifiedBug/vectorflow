7be0e6bd0dd0dbe1bd104d3ee58a03c01636f7c9
TerrifiedBug <35064668+TerrifiedBug@users.noreply.github.com>
2026-02-27 18:43:22 +0000

    feat: add pipeline tRPC router with graph save

--- src/trpc/router.ts
+++ src/trpc/router.ts
@@ -2,11 +2,13 @@ import { router } from "./init";
 import { teamRouter } from "@/server/routers/team";
 import { environmentRouter } from "@/server/routers/environment";
 import { fleetRouter } from "@/server/routers/fleet";
+import { pipelineRouter } from "@/server/routers/pipeline";
 
 export const appRouter = router({
   team: teamRouter,
   environment: environmentRouter,
   fleet: fleetRouter,
+  pipeline: pipelineRouter,
 });
 
 export type AppRouter = typeof appRouter;
